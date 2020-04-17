a2021c6d78341981d8172a0561125037edb7cb5d
Brice808 <63297020+Brice808@users.noreply.github.com>
2020-04-17 16:28:09 +0200

    Update morpionmax.py

--- morpionmax.py
+++ morpionmax.py
@@ -1,6 +1,7 @@
 a = [[''for x in range (3)]for y in range (3)]
 joueur1 = 'x'
 joueur2 = 'o'
+
 print (a)
 while tour !=4:
     a = [[''for x in range (3)]for y in range (3)]
@@ -8,5 +9,9 @@ while tour !=4:
     a = input ()
     print('votre choix' + a)
     j1+=1
-
+    print ('saisissez votre choix x et y de coordonées pour le tour')
+    a = input ()
+    print('votre choix' + a)
+    j2+=1
+    tour+=1
     
